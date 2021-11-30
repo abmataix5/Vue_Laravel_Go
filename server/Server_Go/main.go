1fd1f1a75d2f9b6ff05793ac47bfde65021a86bc
mataix.ab5 <mataix.ab@gmail.com>
2021-11-30 21:40:36 +0100

    Delete Old Go server

--- server/Server_Go/main.go
+++ server/Server_Go/main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"first-api/Config"
-	"first-api/Models"
-	"first-api/Routes"
-	"fmt"
-
-	"github.com/gin-contrib/cors"
-	"github.com/jinzhu/gorm"
+	"github.com/victorsteven/forum/api"
 )
 
-var err error
-
 func main() {
-	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
 
-	if err != nil {
-		fmt.Println("Status:", err)
-	}
+	api.Run()
 
-	defer Config.DB.Close()
-	Config.DB.AutoMigrate(&Models.Worker{})
-
-	r := Routes.SetupRouter()
-
-	/* Cors default * */
-	r.Use(cors.Default())
-
-	//Running
-	r.Run(":3040")
 }
